d35469890183d8a21b40ad6eca3d62571dbb5d23
Student <example@example.com>
2019-07-15 15:21:43 -0700

    Updates

---
+++ precache-manifest.7893ca7dddc87710302fbe0f9c70e51f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63296210560d07a3ec4379b0099b38fe",
+    "url": "/Tamagotchi/index.html"
+  },
+  {
+    "revision": "b7b4e6ddb6ab84803233",
+    "url": "/Tamagotchi/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Tamagotchi/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "b7b4e6ddb6ab84803233",
+    "url": "/Tamagotchi/static/js/main.f3b9f493.chunk.js"
+  },
+  {
+    "revision": "044a88ad237d68e4c8e5",
+    "url": "/Tamagotchi/static/js/runtime~main.b27164d3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Tamagotchi/static/media/logo.5d5d9eef.svg"
+  }
+]);
